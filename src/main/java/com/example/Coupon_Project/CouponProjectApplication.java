646b051ef268a5d4a17ec7fa7a5cec5acf4208e1
Sergey Dragoner <acedragoner777@gmail.com>
2023-04-03 01:03:18 +0300

    Prepared the CustomerServices and CompanyServices

--- src/main/java/com/example/Coupon_Project/CouponProjectApplication.java
+++ src/main/java/com/example/Coupon_Project/CouponProjectApplication.java
@@ -1,19 +1,13 @@
 package com.example.Coupon_Project;
 
-import com.example.Coupon_Project.beans.Company;
-import com.example.Coupon_Project.beans.Customer;
 import com.example.Coupon_Project.exceptions.login.ClientInfoIncorrectException;
-import com.example.Coupon_Project.exceptions.companies.CompanyAlreadyExistException;
-import com.example.Coupon_Project.exceptions.companies.CompanyDoesntExistException;
-import com.example.Coupon_Project.exceptions.customers.CustomerAlreadyExistException;
-import com.example.Coupon_Project.services.Administrator;
 import com.example.Coupon_Project.services.ClientManager.ClientType;
+import com.example.Coupon_Project.services.CompanyServices;
+import com.example.Coupon_Project.services.CustomerServices;
 import com.example.Coupon_Project.services.LoginManager.Login;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-
-import java.util.ArrayList;
 
 @SpringBootApplication
 public class CouponProjectApplication {
@@ -22,22 +16,41 @@ public class CouponProjectApplication {
 
 		ConfigurableApplicationContext ctx =  SpringApplication.run(CouponProjectApplication.class, args);
 		Login log = ctx.getBean(Login.class);
-		try {
-			Administrator admin = (Administrator) log.login("admin@admin.com", "admin", ClientType.Administrator);
 
-			Company cust = admin.getOneCompany(1);
-			admin.deleteCustomer(1);
-			System.out.println(cust);
-			ArrayList<Customer> customers = (ArrayList<Customer>) admin.getAllCustomers();
-			System.out.println(customers);
-			admin.addCustomer(new Customer("Sergey", "Dragoner", "lalalala@gmail.com", "1234"));
-			admin.addCompany(new Company("Nike", "JustDoIt@Adidas.das.com", "1234"));
-		} catch (ClientInfoIncorrectException | CompanyAlreadyExistException | CustomerAlreadyExistException |
-				 CompanyDoesntExistException e) {
+		//Administrator -> --> --->
+//		try {
+//			Administrator admin = (Administrator) log.login("admin@admin.com", "admin", ClientType.Administrator);
+//
+//			Company cust = admin.getOneCompany(1);
+//			admin.deleteCustomer(1);
+//			System.out.println(cust);
+//			ArrayList<Customer> customers = (ArrayList<Customer>) admin.getAllCustomers();
+//			System.out.println(customers);
+//			admin.addCustomer(new Customer("Sergey", "Dragoner", "lalalala@gmail.com", "1234"));
+//			admin.addCompany(new Company("Nike", "JustDoIt@Adidas.das.com", "1234"));
+//		} catch (ClientInfoIncorrectException | CompanyAlreadyExistException | CustomerAlreadyExistException |
+//				 CompanyDoesntExistException e) {
+//			System.out.println(e.getMessage());
+//		}
+
+
+		//Company -> --> --->
+		try {
+			CompanyServices company = (CompanyServices) log.login("JustDo", "ad4u", ClientType.Company);
+
+
+		} catch (ClientInfoIncorrectException e) {
 			System.out.println(e.getMessage());
 		}
 
+		//Customer -> --> -->
+		try {
+			CustomerServices customer = (CustomerServices) log.login("lalalala@gmail.com", "1234", ClientType.Customer);
 
+
+		} catch (ClientInfoIncorrectException e) {
+			System.out.println(e.getMessage());
+		}
 
 	}
 
