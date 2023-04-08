a2468d2781d5abef9638297074dc2f6bb5fd9c3d
Sergey Dragoner <acedragoner777@gmail.com>
2023-04-08 21:56:08 +0300

    I

--- src/main/java/com/example/Coupon_Project/CouponProjectApplication.java
+++ src/main/java/com/example/Coupon_Project/CouponProjectApplication.java
@@ -12,7 +12,6 @@ public class CouponProjectApplication {
 	public static void main(String[] args) {
 
 		ConfigurableApplicationContext ctx =  SpringApplication.run(CouponProjectApplication.class, args);
-
 	}
 
 }
